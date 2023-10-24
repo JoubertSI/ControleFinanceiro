05a111e60fb14605e829c7cc5c4970da3f06e0e5
JOUBERT PINHEIRO <joubert.pinheiro@outlook.com>
2023-10-24 20:44:36 -0300

    Atualização da Views

--- Controllers/InstituicaoController.cs
+++ Controllers/InstituicaoController.cs
@@ -1,12 +1,31 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SistemaFinanceiroUGB232.Models;
 
 namespace SistemaFinanceiroUGB232.Controllers
 {
     public class InstituicaoController : Controller
     {
+        private static IList<Instituicao> instituicoes = new List<Instituicao>()
+        {
+            new Instituicao ()
+            {
+                InstituicaoID = 1,
+                Nome =  "Hogwarts",
+                Endereco = "Escócia"
+            },
+
+            new Instituicao ()
+            {
+                InstituicaoID = 2,
+                Nome = "Mansao X",
+                Endereco = "Nova York"
+            }
+
+
+        };
         public IActionResult Index()
         {
-            return View();
+            return View(instituicoes);
         }
     }
 }
