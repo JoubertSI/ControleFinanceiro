8c0cec05e6a6fcfef071c3c2c79f3ee5b05a0229
JOUBERT PINHEIRO <joubert.pinheiro@outlook.com>
2023-10-24 20:25:36 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/InstituicaoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SistemaFinanceiroUGB232.Controllers
+{
+    public class InstituicaoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/Instituicao.cs
@@ -0,0 +1,10 @@
+﻿namespace SistemaFinanceiroUGB232.Models
+{
+    public class Instituicao
+    {
+        public long? InstituicaoID { get; set; }
+        public string Nome { get; set; }
+
+        public string Endereco { get; set; }
+    }
+}
